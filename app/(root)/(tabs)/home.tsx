a52a2556c92526c600711583ec63fbcbdb93e5db
Nmn <Naman.tortalabz@hotmail.com>
2024-10-03 21:20:10 +0530

    auth screens , clerk email authentication and neondb setup completed

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,13 +1,24 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
+import { Link } from 'expo-router'
+import { Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
-const HomeScreen = () => {
+export default function Page() {
+  const { user } = useUser()
+
   return (
     <SafeAreaView>
-      <Text>HomeScreen</Text>
+      <SignedIn>
+        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+      </SignedIn>
+      {/* <SignedOut>
+        <Link href="/sign-in">
+          <Text>Sign In</Text>
+        </Link>
+        <Link href="/sign-up">
+          <Text>Sign Up</Text>
+        </Link>
+      </SignedOut> */}
     </SafeAreaView>
   )
 }
-
-export default HomeScreen
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,21 +4,25 @@ import { Stack } from 'expo-router'
 import * as SplashScreen from 'expo-splash-screen'
 import { useEffect } from 'react'
 import 'react-native-reanimated'
-
+import { Slot } from 'expo-router'
+import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo'
+import { tokenCache } from '@/lib/auth'
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync()
 
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
 const MainLayout = () => {
 	const [loaded] = useFonts({
-    "Jakarta-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
-    "Jakarta-ExtraBold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
-    "Jakarta-ExtraLight": require("../assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
-    "Jakarta-Light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
-    "Jakarta-Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
-    "Jakarta-Regular": require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
-    "Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
-  });
+		'Jakarta-Bold': require('../assets/fonts/PlusJakartaSans-Bold.ttf'),
+		'Jakarta-ExtraBold': require('../assets/fonts/PlusJakartaSans-ExtraBold.ttf'),
+		'Jakarta-ExtraLight': require('../assets/fonts/PlusJakartaSans-ExtraLight.ttf'),
+		'Jakarta-Light': require('../assets/fonts/PlusJakartaSans-Light.ttf'),
+		'Jakarta-Medium': require('../assets/fonts/PlusJakartaSans-Medium.ttf'),
+		'Jakarta-Regular': require('../assets/fonts/PlusJakartaSans-Regular.ttf'),
+		'Jakarta-SemiBold': require('../assets/fonts/PlusJakartaSans-SemiBold.ttf'),
+	})
 
 	useEffect(() => {
 		if (loaded) {
@@ -26,18 +30,28 @@ const MainLayout = () => {
 		}
 	}, [loaded])
 
+	// if (!publishableKey) {
+	// 	throw new Error(
+	// 		'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
+	// 	)
+	// }
+
 	if (!loaded) {
 		return null
 	}
 
 	return (
-			<Stack>
-				<Stack.Screen name="index" options={{ headerShown: false }} />
-				<Stack.Screen name="(root)" options={{ headerShown: false }} />
-				<Stack.Screen name="(auth)" options={{ headerShown: false }} />
-				<Stack.Screen name="+not-found" />
-			</Stack>
+		<ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache} >
+			<ClerkLoaded>
+				<Stack>
+					<Stack.Screen name="index" options={{ headerShown: false }} />
+					<Stack.Screen name="(root)" options={{ headerShown: false }} />
+					<Stack.Screen name="(auth)" options={{ headerShown: false }} />
+					<Stack.Screen name="+not-found" />
+				</Stack>
+			</ClerkLoaded>
+		</ClerkProvider>
 	)
 }
 
-export default MainLayout;
+export default MainLayout
